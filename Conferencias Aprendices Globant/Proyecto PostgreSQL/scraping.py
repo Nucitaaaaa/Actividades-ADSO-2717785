d71ed7b8a8738ad7cdbb446682db232b8cb1a652
SL234 <123673043+Nucitaaaaa@users.noreply.github.com>
2023-05-13 08:21:11 -0500

    Update scraping.py

--- Conferencias Aprendices Globant/Proyecto PostgreSQL/scraping.py
+++ Conferencias Aprendices Globant/Proyecto PostgreSQL/scraping.py
@@ -1,1 +1,16 @@
+#import_librerias
+from bs4 import BeautifulSoup
+import requests
+import json 
 
+def get_html_info(base_url,pages_site):
+
+    items = []
+
+    for n in range(1, pages_site):
+
+        url = f"{base_url}page={n}"
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        items_found = soup.find_all('div', class_= 'infor')
+        items.extend(items_found)
