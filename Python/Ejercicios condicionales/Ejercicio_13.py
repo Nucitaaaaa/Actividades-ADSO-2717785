94c8ae6c585fcf399cb1f6c28141d87c9d1e4077
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-05-19 16:04:40 -0500

    Update Ejercicio_13.py

--- Python/Ejercicios condicionales/Ejercicio_13.py
+++ Python/Ejercicios condicionales/Ejercicio_13.py
@@ -1,1 +1,15 @@
 
+"""
+Dado tres números calcular el menor
+"""
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+num3 = int(input("Enter one last number: "))
+
+if num1 < num2 and num3:
+    print(f"{num1} is the smallest number")
+elif num2 < num1 and num3:
+    print(f"{num2} is the smallest number")
+elif num3 < num1 and num2:
+    print(f"{num3} is the smallest number")
