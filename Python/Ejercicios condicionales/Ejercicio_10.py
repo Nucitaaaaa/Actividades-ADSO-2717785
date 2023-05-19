0290d002398b99a9986d5973d68be0127a9f818c
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-05-19 15:47:48 -0500

    add and changes

--- Python/Ejercicios condicionales/Ejercicio_10.py
+++ Python/Ejercicios condicionales/Ejercicio_10.py
@@ -3,3 +3,14 @@
 Mostrar la suma de dos números enteros, si el resultado supera a 10
 """
 
+num1 = int(input("Enter your first num: "))
+num2 = int(input("Enter your second num: "))
+
+sum = num1 + num2
+
+if sum > 10:
+    print(f"the result is {sum}")
+else:
+    print("the result must be greater than 10")
+
+    
