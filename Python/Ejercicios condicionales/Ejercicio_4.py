90c7fe114967af6609ee39ea951dbeccdbd3d29c
SL234 <123673043+Nucitaaaaa@users.noreply.github.com>
2023-05-19 10:43:44 -0500

    Create Ejercicio_4.py

---
+++ Python/Ejercicios condicionales/Ejercicio_4.py
@@ -0,0 +1,17 @@
+
+"""
+Diseña un algoritmo que lea 2 números y visualice si son positivos
+"""
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter other number: "))
+
+if num1 > 0:
+    print(f"The number {num1} is positive")
+else:
+    print(f"The number {num1} is negative")
+
+if num2 > 0:
+    print(f"The number {num2} is positive")
+else:
+    print(f"The number {num2} is negative")
