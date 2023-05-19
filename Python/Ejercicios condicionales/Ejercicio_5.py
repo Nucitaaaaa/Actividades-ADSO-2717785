019ea1bcdc5d1a9a0a13540f166bb73d27eb1761
SL234 <123673043+Nucitaaaaa@users.noreply.github.com>
2023-05-19 11:13:37 -0500

    update and add

---
+++ Python/Ejercicios condicionales/Ejercicio_5.py
@@ -0,0 +1,24 @@
+"""
+Un hombre desea saber cuánto dinero se genera por concepto de intereses
+en relación la cantidad que tiene en inversión en el banco
+
+El decidirá reinvertir los intereses siempre y cuando estos no excedan a $7000,
+y en ese caso diseña un algoritmo para saber cuánto dinero tendrá
+finalmente en su cuenta.
+"""
+
+investment  = float(input("enter your initial investment: "))
+interest = float(input("enter the interest rate: "))
+
+reinvestment = investment * interest
+
+if investment <= 7000:
+    finalInvestment = investment + reinvestment
+    print(f"the final amount of money in your account is {finalInvestment}")
+
+else:
+    finalInvestment = investment + 7000
+    print(f"the final amount of money in your account is {finalInvestment}")
+
+
+
