560c2ac1750f9e173e8a6b0ca241bd96a08016b4
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-05-19 18:51:06 -0500

    add and changes

--- Python/Ejercicios condicionales/Ejercicio_27.py
+++ Python/Ejercicios condicionales/Ejercicio_27.py
@@ -4,29 +4,29 @@ Realice un menú donde el usuario deberá seleccionar una opción de las
 operaciones básicas, y se le solicite al usuario digitar dos números, y
 dependiendo de la respuesta realice cada operación
 """
-print("Calculadora Basica\n")
-print("Digite:\n\n1 Para suma\n2 Para resta\n3 Para multiplicación\n4 Para división")
+print("basic calculatorn")
+print("Enter:\n\n1 for addition\n2 for subtraction\n3 for multiplication\n4 for division")
 
-operacion = int(input("\nDigite el numero de la operación a realizar: "))
+operation = int(input("\nEnter the number of the operation to be carried out: "))
 
-num1 = int(input("Digite el primer numero: "))
-num2 = int(input("Digite el segundo numero: "))
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
-if operacion == 1:
-    suma = num1 + num2
-    print("\nEl resultado de la suma es:", suma)
+if operation == 1:
+    summ = num1 + num2
+    print("\nThe result of the addition is:", summ)
     print(" ")
-elif operacion == 2:
-    resta = num1 - num2
-    print("\nEl resultado de la resta es:", resta)
+elif operation == 2:
+    sub = num1 - num2
+    print("\nThe result of the subtraction is:", sub)
     print(" ")
-elif operacion == 3:
+elif operation == 3:
     multi = num1 * num2
-    print("\nEl resultado de la multiplicación es:", multi)
+    print("\nThe result of the multiplication is:", multi)
     print(" ")
-elif operacion == 4:
+elif operation == 4:
     divi = num1 / num2
-    print("\nEl resultado de la división es:", divi)
+    print("\nThe result of the division is:", divi)
     print(" ")
 else:
-    print("Digite un numero de operación valido")
+    print("Enter a valid transaction number")
---
+++ Python/Ejercicios condicionales/Ejercicio_28.py
@@ -0,0 +1,10 @@
+
+"""
+La tienda sofia valida el valor de la compra si el valor de la compra es mayor
+a 40 mil , si es así debe indicar el color de la balota(roja 10%, blanca 15%,
+negra 20%,amarilla 20%, verde 30%) según el color se aplica el descuento, se
+debe mostrar el descuento y el valor neto a pagar.
+Si los valores son menores que 40 no va aplicar ningún descuento.
+"""
+
+
