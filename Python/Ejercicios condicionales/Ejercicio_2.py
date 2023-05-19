6eae4af6dbc4fdd17029d078e0aad56131601815
SL234 <123673043+Nucitaaaaa@users.noreply.github.com>
2023-05-19 10:04:28 -0500

    add

---
+++ Python/Ejercicios condicionales/Ejercicio_2.py
@@ -0,0 +1,12 @@
+
+"""
+Diseñe un algoritmo que, al ingresar la edad del usuario, si es mayor de edad
+mostrar el un mensaje “es mayor de edad”
+"""
+
+age = int(input("Ingrese su edad: "))
+
+if age >= 18:
+    print("Es mayor de edad")
+else:
+    print("Es menor de edad")
---
+++ Python/Ejercicios condicionales/Ejercicio_3.py
@@ -0,0 +1,9 @@
+
+"""
+Diseñe un algoritmo que guarde el nombre del aprendiz, nombre del curso,
+nota definitiva, el nro de clases en el semestre y el número de las fallas. En el
+caso de que las fallas superen el 30% del número de clases se debe mostrar
+la nota que no aprobó y se calificara cero(0)
+"""
+
+name = str(input(""))
