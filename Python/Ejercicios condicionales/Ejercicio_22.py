aab473498444e21be02442b8f132257ffb482719
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-05-19 17:31:41 -0500

    Add

--- Python/Ejercicios condicionales/Ejercicio_22.py
+++ Python/Ejercicios condicionales/Ejercicio_22.py
@@ -1,4 +1,24 @@
 
 """
-
+A un trabajador le descuentan de su sueldo el 4%, si su sueldo es menor o
+igual a $1000000, si el sueldo está entre $1000000 y $2000000 el 7%, y por
+encima de 2000000 el 8% calcular el descuento y sueldo neto que recibe el
+trabajador dado su sueldo.
 """
+
+salary = int(input("Enter your salary: "))
+
+if salary <= 1000000:
+    discount = salary * 0.04
+    salary -= discount
+    print(f"Your salary will be ${salary}, with a discount of {discount}")
+elif salary > 1000000 and salary < 2000000:
+    discount = salary * 0.07
+    salary -= discount
+    print(f"Your salary will be ${salary} with a discount of {discount}")
+elif salary >= 2000000:
+    discount = salary * 0.08
+    salary -= discount
+    print(f"Your salary will be ${salary} with a discount of {discount}")
+else:
+    print("Not apply...")
---
+++ Python/Ejercicios condicionales/Ejercicio_23.py
@@ -0,0 +1,7 @@
+
+"""
+Un cliente va a comprar una moto y se percata que si lo compraba el día
+martes tiene un descuento del 10%, luego si lo compra el día sábado tiene
+un descuento del 18% mostrar cuanto pagara en cada opción
+"""
+
