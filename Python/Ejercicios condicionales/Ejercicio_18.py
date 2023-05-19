9c8ee394ba8decd17dc5e7792d8af591db8a7e9c
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-05-19 16:46:48 -0500

    Update Ejercicio_18.py

--- Python/Ejercicios condicionales/Ejercicio_18.py
+++ Python/Ejercicios condicionales/Ejercicio_18.py
@@ -4,3 +4,11 @@ Hacer un programa que pida 3 números e indicar si el tercero es igual a la
 suma del primero y el segundo.
 """
 
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+num3 = int(input("Enter one last number: "))
+
+if num3 == num1 + num2:
+    print(f"The number \"{num3}\" is the sum of {num1} and {num2}")
+else:
+    print(f"The number \"{num3}\" is not the sum of {num1} and {num2}")
