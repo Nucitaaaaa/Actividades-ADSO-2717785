62b939a404321202017882fdf6f3198260df1f6d
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-05-19 19:34:55 -0500

    Create Ejercicio_30.py

---
+++ Python/Ejercicios condicionales/Ejercicio_30.py
@@ -0,0 +1,8 @@
+
+"""
+Realizar el siguiente algoritmo:
+Entrada: un numero, que se almacenará en una variable llamada numeroDia.
+Proceso: Realizar las respectivas comparaciones para cada uno de los valores entre 1 y 7.
+Salida: Mostrar el nombre del dia, segun el valor contenido en numeroDia.
+"""
+
