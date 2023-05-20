2944a8452768776054dcb0f220eb122db15395f3
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-05-19 19:45:15 -0500

    Create Ejercicio_31.py

---
+++ Python/Ejercicios condicionales/Ejercicio_31.py
@@ -0,0 +1,11 @@
+
+"""
+Realizar el siguiente algoritmo
+Calcular el salario de un empleado dado el numero de horas, el valor de la hora y la categoria del empleado.
+Categoria:
+A: 10% de incremento
+B: 15% de incremento
+C: 23% de incremento
+D: 25% de incremento
+"""
+
