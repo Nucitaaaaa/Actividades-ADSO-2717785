45747845c7d5dd0d515dda9258171b26681bdcfd
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-05-19 19:41:24 -0500

    Update Ejercicio_30.py

--- Python/Ejercicios condicionales/Ejercicio_30.py
+++ Python/Ejercicios condicionales/Ejercicio_30.py
@@ -6,3 +6,21 @@ Proceso: Realizar las respectivas comparaciones para cada uno de los valores ent
 Salida: Mostrar el nombre del dia, segun el valor contenido en numeroDia.
 """
 
+dayNum = int(input("Write a number from 1 to 7 and I will tell you what day of the week it is: "))
+
+if dayNum == 1:
+    print("The day of the week is Monday")
+elif dayNum == 2:
+    print("The day of the week is Tuesday")
+elif dayNum == 3:
+    print("The day of the week is Wednesday")
+elif dayNum == 4:
+    print("The day of the week is Thursday")
+elif dayNum == 5:
+    print("The day of the week is Friday") 
+elif dayNum == 6:
+    print("The day of the week is Saturday")
+elif dayNum == 7:
+    print("The day of the week is Sunday")
+else:
+    print("That number is not a day of the week")
