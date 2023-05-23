c2dfc74de5a8a3473bb36b35a75b1532d234a63b
SL234 <123673043+Nucitaaaaa@users.noreply.github.com>
2023-05-23 10:22:31 -0500

    Update Ejercicio_25.py

--- Python/Ejercicios condicionales/Ejercicio_25.py
+++ Python/Ejercicios condicionales/Ejercicio_25.py
@@ -16,5 +16,15 @@ q3 = str(input("has a mask ?: "))
 
 if q1 == "YES" or q1 == "yes" and q2 == "YES" or q2 == "yes" and q3 == "YES" or q3 == "yes":
     print("¡Your superhero is Iroman!")
+elif q1 == "YES" or q1 == "yes" and q2 == "YES" or q2 == "yes" and q3 == "NO" or q3 == "NO":
+    print("¡Your superhero is Captain Marvel!")
+elif q1 == "YES" or q1 == "yes" and q2 == "NO" or q2 == "no" and q3 == "NO" or q1 == "no":
+    print("¡Your superhero is Thor!")
 else:
     print("Oh, I dont know who is he/she :c")
+
+
+
+
+
+
