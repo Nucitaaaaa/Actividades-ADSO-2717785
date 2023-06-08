106ff028a6578b1c5459593fc7a98b0543e22480
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-06-07 21:50:34 -0500

    changes

--- Conferencias Aprendices Globant/ProyectoReact/src/Components/Footer.jsx
+++ Conferencias Aprendices Globant/ProyectoReact/src/Components/Footer.jsx
@@ -6,8 +6,10 @@ const Footer = () => {
 
 return (
     <>
-        <p className='pFooter'>Para mas información sobre react, has click <a className='link' href="https://es.react.dev/" target = "_blank">en este enlace</a></p>
-        <p className='pFooter2'><strong>Copyright © 2023 Todos los derechos reservados</strong></p>
+        <footer>
+            <p className='pFooter'>Para mas información sobre react, has click <a className='link' href="https://es.react.dev/" target = "_blank">en este enlace</a></p>
+            <p className='pFooter2'><strong>Copyright © 2023 Todos los derechos reservados</strong></p>
+        </footer>
     </>
 )
 
