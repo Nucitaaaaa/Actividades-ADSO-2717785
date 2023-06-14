aec48822835565b396d90ff84f82b1698299db57
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-06-14 17:21:49 -0500

    Update app.js

--- Conferencias Aprendices Globant/ProyectoNode.js/app.js
+++ Conferencias Aprendices Globant/ProyectoNode.js/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 3030;
 app.use(express.json());
+app.use(express.static('public'));
 
 const estudiantes = [
     { id: 1, nombre: "Miguel", apellido: "Perdomo", edad: 19, semestre: 2, estudia: true },
@@ -14,14 +15,110 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/estudiantes", (req, res) => {
-    res.send(estudiantes);
+    const estudiantesHTML = estudiantes.map(estudiante => `
+        <tr>
+            <td>${estudiante.id}</td>
+            <td>${estudiante.nombre}</td>
+            <td>${estudiante.apellido}</td>
+            <td>${estudiante.edad}</td>
+            <td>${estudiante.semestre}</td>
+            <td>${estudiante.estudia ? 'Sí' : 'No'}</td>
+        </tr>
+    `).join('');
+
+    const html = `
+        <html>
+        <head>
+            <title>Tabla de Estudiantes</title>
+            <style>
+                table {
+                    border-collapse: collapse;
+                    width: 100%;
+                }
+        
+                th, td {
+                    border: 1px solid black;
+                    padding: 8px;
+                    text-align: left;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Tabla de Estudiantes</h1>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Edad</th>
+                    <th>Semestre</th>
+                    <th>Estudia</th>
+                </tr>
+                ${estudiantesHTML}
+            </table>
+        </body>
+        </html>
+    `;
+
+    res.send(html);
 });
 
 app.get("/api/estudiantes/:id", (req, res) => {
-    const alumno = estudiantes.find((e) => e.id === parseInt(req.params.id));
-    if (!alumno) return res.status(404).send("Estudiantes no encontrado");
-    else res.send(alumno);
+    const id = parseInt(req.params.id);
+    const estudiante = estudiantes.find(est => est.id === id);
+
+    if (estudiante) {
+        const estudianteHTML = `
+            <tr>
+                <td>${estudiante.id}</td>
+                <td>${estudiante.nombre}</td>
+                <td>${estudiante.apellido}</td>
+                <td>${estudiante.edad}</td>
+                <td>${estudiante.semestre}</td>
+                <td>${estudiante.estudia ? 'Sí' : 'No'}</td>
+            </tr>
+        `;
+
+        const html = `
+            <html>
+            <head>
+                <title>Tabla de Estudiantes</title>
+                <style>
+                    table {
+                        border-collapse: collapse;
+                        width: 100%;
+                    }
+
+                    th, td {
+                        border: 1px solid black;
+                        padding: 8px;
+                        text-align: left;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>Detalles del estudiante</h1>
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Edad</th>
+                        <th>Semestre</th>
+                        <th>Estudia</th>
+                    </tr>
+                    ${estudianteHTML}
+                </table>
+            </body>
+            </html>
+        `;
+
+        res.send(html);
+    } else {
+        res.status(404).send("Estudiante no encontrado");
+    }
 });
+
 
 app.post("/api/estudiantes", (req, res) => {
     const alum = {
@@ -63,6 +160,7 @@ app.delete("/api/estudiantes/:id", (req, res) => {
     estudiantes.splice(index, 1);
     res.send(alumno);
 });
+
 
 app.listen(port, () => console.log(`Escuchando al puerto ${port}....`));
 
