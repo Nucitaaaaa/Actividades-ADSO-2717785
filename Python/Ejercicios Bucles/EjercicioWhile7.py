6a87d19e022b514fa15178622fc317166bbc6fb7
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-06-17 12:37:25 -0500

    Update EjercicioWhile7.py

--- Python/Ejercicios Bucles/EjercicioWhile7.py
+++ Python/Ejercicios Bucles/EjercicioWhile7.py
@@ -1,2 +1,12 @@
 
-#*
+#*Leer números enteros del teclado, hasta que el usuario ingrese el 0. Finalmente, mostrar la sumatoria de todos los números ingresados.
+
+end = 0
+suma = 0
+
+while not end:
+    n = int(input("Ingrese todos los numeros enteros del teclado: "))
+    suma += n
+    if n == end:
+        print(f"La suma de los numeros del teclado ingresados es {suma}")
+        break
