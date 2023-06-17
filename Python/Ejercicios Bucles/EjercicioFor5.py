dd8f2ea98525fc515277ad7f60700a895a448b3f
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-06-16 20:00:19 -0500

    Update EjercicioFor5.py

--- Python/Ejercicios Bucles/EjercicioFor5.py
+++ Python/Ejercicios Bucles/EjercicioFor5.py
@@ -3,15 +3,15 @@
 
 def odd():
     n = int(input("Digite un numero: "))
-    acum = 0
+    cont = 0
     suma = 0
 
     for i in range(0, n + 1):
         if i % 2 != 0:
-            suma = i + acum
-            acum += 1
+            suma += i
+            cont += 1
     
     print(f"La suma de todos los numeros impares que hay desde 0 hasta {n} es: {suma}")
-    print(f"Los numeros impares que hay desde 0 hasta {n} son: {acum}")
+    print(f"Los numeros impares que hay desde 0 hasta {n} son: {cont}")
 
 odd()
