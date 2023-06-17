5e1db8fa51044c30b1747706cfa6c21452c3d9bc
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-06-17 09:01:04 -0500

    Update EjercicioFor11.py

--- Python/Ejercicios Bucles/EjercicioFor11.py
+++ Python/Ejercicios Bucles/EjercicioFor11.py
@@ -1,6 +1,8 @@
 
 #*Elabore un algoritmo que pregunte cuántas temperaturas se van a introducir, pida esas temperaturas, y escriba la temperatura más alta, la más baja y la temperatura promedio.
 
+#*Forma 1
+
 numTemp = int(input("Ingrese el numero de temperaturas quiere ingresar: "))
 temps = []
 prom = 0
@@ -21,7 +23,34 @@ print(f"La temperatura promedio es {prome}")
 
 
 
+#*Forma 2
 
+numTemp = int(input("Ingrese el numero de temperaturas quiere ingresar: "))
+count = 0
+acum = 0
+
+for i in range(numTemp):
+    print(f"Ingrese la {i} temperatura:", end=" ")
+    temp = float(input())
+
+    if count == 0:
+        maxi = temp 
+        mini = temp
+    else:
+        if temp > maxi:
+            maxi = temp
+        elif temp < mini:
+            mini = temp
     
+    acum += temp
+    count += 1
+
+prome = acum / numTemp
+
+print(f"La temperatura mayor es: {maxi}")
+print(f"La temperatura menor es: {mini}")
+print(f"La temperatura promedio es {prome}")
+
+
 
 
