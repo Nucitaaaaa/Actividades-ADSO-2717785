62903e153af707eb6f0ccbad65ea007a3b6fa3cc
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-06-17 12:53:47 -0500

    Create FALTAEjercicioWhile8.py

---
+++ Python/Ejercicios Bucles/FALTAEjercicioWhile8.py
@@ -0,0 +1,5 @@
+
+#*Elabore un algoritmo que pida un número entero mayor que cero y que escriba sus divisores. Validar que el usuario haya ingresado un número mayor a cero.
+
+num = int(input("Digite un numero MAYOR a 0: "))
+
