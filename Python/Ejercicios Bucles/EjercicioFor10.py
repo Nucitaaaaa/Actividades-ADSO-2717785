47eab3ecbc4f8232fcf4c79be400b2d46aad10b2
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-06-17 08:25:48 -0500

    Update EjercicioFor10.py

--- Python/Ejercicios Bucles/EjercicioFor10.py
+++ Python/Ejercicios Bucles/EjercicioFor10.py
@@ -4,8 +4,8 @@
 n = int(input("Ingrese un numero MAYOR A 0: "))
 
 if n > 0:
-    for i in range(1,50): #*Puse 50 como numero limite porque no se especificaba uno en el ejercicio.
-        if i % n == 0:
+    for i in range(1,n + 1): 
+        if n % i == 0:
             print(f"{i} es divisor de {n}")
 else:
     print("El numero debe ser MAYOR a 0...")
