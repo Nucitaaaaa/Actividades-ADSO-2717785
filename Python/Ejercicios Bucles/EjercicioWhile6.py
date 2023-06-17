f1ae19f08267f83328d36ef370a3238eab8c0478
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-06-17 12:19:35 -0500

    add

--- Python/Ejercicios Bucles/EjercicioWhile6.py
+++ Python/Ejercicios Bucles/EjercicioWhile6.py
@@ -1,2 +1,16 @@
 
-#*
+#*Un maestro de escuela necesita un algoritmo que capture n notas de un estudiante y calcule el promedio
+
+end = 0
+notas = []
+suma = 0
+
+while not end:
+    nota = float(input("Ingrese la nota del estudiante aquí: "))
+    salir = input("Desea sacar el promedio ahora? ingrese \"si\" para sacar el promedio: ")
+    notas.append(nota)
+    suma += nota
+    if salir.lower() == "si":
+        prom = suma / len(notas)
+        print(f"El promedio de las notas es de {prom}")
+        break
