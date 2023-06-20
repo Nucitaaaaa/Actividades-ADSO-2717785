91a4aacbfa203206a1b6647d2c90ed7a87736510
SL234 <123673043+Nucitaaaaa@users.noreply.github.com>
2023-06-20 07:28:10 -0500

    Update EjercicioFor12.py

--- Python/Ejercicios Bucles/EjerciciosFor/EjercicioFor12.py
+++ Python/Ejercicios Bucles/EjerciciosFor/EjercicioFor12.py
@@ -1,24 +1,24 @@
 
-# #*Elaborar un algoritmo que pida las 4 notas de n estudiantes e imprima la nota más alta, la más baja y el promedio.
+#*Elaborar un algoritmo que pida las 4 notas de n estudiantes e imprima la nota más alta, la más baja y el promedio.
 
-# #*Forma1
+#*Forma1
 
-# totalNotas = []
-# acum = 0
+totalNotas = []
+acum = 0
 
-# for i in range(1,4+1):
-#     print(f"Ingrese la {i} nota:", end=" ")
-#     nota = float(input())
-#     totalNotas.append(nota)
-#     acum += nota
+for i in range(1,4+1):
+    print(f"Ingrese la {i} nota:", end=" ")
+    nota = float(input())
+    totalNotas.append(nota)
+    acum += nota
 
-# mayor = max(totalNotas)
-# menor = min(totalNotas)
-# prom = acum / len(totalNotas)
+mayor = max(totalNotas)
+menor = min(totalNotas)
+prom = acum / len(totalNotas)
 
-# print(f"La nota mayor es {mayor}")
-# print(f"La nota menor es {menor}")
-# print(f"El promedio de las notas es {prom}")
+print(f"La nota mayor es {mayor}")
+print(f"La nota menor es {menor}")
+print(f"El promedio de las notas es {prom}")
 
 
 #*Forma2
