2dcb293d0cc6ba8890129d58572a49ca2bd1543c
SL234 <123673043+Nucitaaaaa@users.noreply.github.com>
2023-06-21 08:34:29 -0500

    Update EjerciciosWhile.py

--- Python/Ejercicios Bucles/Ejercicios Bucles En Clase/EjerciciosWhile.py
+++ Python/Ejercicios Bucles/Ejercicios Bucles En Clase/EjerciciosWhile.py
@@ -19,4 +19,32 @@ print("Obediente :D")
 """
 
 #3
+"""
+name = str(input("Digite un nombre: "))
+names = []
+count = 1
+
+while True:
+    names.append(name)
+    name = print(f"Digite el {count + 1} nombre, si quiere terminar de introducir, digite Salir: ", end=" ")
+    name = input()
+
+    count += 1
+
+    if (name.strip().upper() == "SALIR"):
+        break
+
+print(f"Usted ha introducido {count - 1} nombres! estos son: \n {names}")
+"""
+
+#4
+
+count = 0
+
+while count <= 10:
+    print(count)
+    count += 1
+
+    if count == 8:
+        break
 
