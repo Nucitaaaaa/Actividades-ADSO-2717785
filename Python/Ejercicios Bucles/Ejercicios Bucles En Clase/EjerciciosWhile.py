99480770196b2efbe157bc429d63006116e76154
SL234 <123673043+Nucitaaaaa@users.noreply.github.com>
2023-06-21 07:17:23 -0500

    Create EjerciciosWhile.py

---
+++ Python/Ejercicios Bucles/Ejercicios Bucles En Clase/EjerciciosWhile.py
@@ -0,0 +1,22 @@
+
+#1
+"""
+count = 0
+
+while count < 4:
+    print(count)
+    count += 1
+"""
+
+#2
+"""
+res = input("Digite 1 o 5 para continuar: ")
+
+while not(res.strip() == "1" or res.strip() == "5"):
+    res = int(input("Digite 1 o 5 para continuar: "))
+
+print("Obediente :D")
+"""
+
+#3
+
