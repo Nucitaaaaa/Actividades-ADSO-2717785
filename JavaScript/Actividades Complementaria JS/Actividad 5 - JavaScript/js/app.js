cea940f0a6aa69ab47bb3a5e4daf477e8a626e13
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-10-16 14:23:49 -0500

    Update app.js

--- JavaScript/Actividades Complementaria JS/Actividad 5 - JavaScript/js/app.js
+++ JavaScript/Actividades Complementaria JS/Actividad 5 - JavaScript/js/app.js
@@ -1,21 +1,23 @@
 
 function calcularPrecio() {
     
-var precioComida = parseInt(document.getElementById("selectPlato").value)
-var cantidadComida = parseInt(document.getElementById("cantidadPLato").value)
-var precioBebida = parseInt(document.getElementById("selectBebida").value)
-var cantidadBebida = parseInt(document.getElementById("cantidadBebida").value)
-var precioPostre = parseInt(document.getElementById("selectPostre").value)
-var cantidadPostre = parseInt(document.getElementById("cantidadPostre").value)
+    var precioComida = parseInt(document.getElementById("selectPlato").value)
+    var cantidadComida = parseInt(document.getElementById("cantidadPLato").value)
+    var precioBebida = parseInt(document.getElementById("selectBebida").value)
+    var cantidadBebida = parseInt(document.getElementById("cantidadBebida").value)
+    var precioPostre = parseInt(document.getElementById("selectPostre").value)
+    var cantidadPostre = parseInt(document.getElementById("cantidadPostre").value)
 
-var precioTotal = document.getElementById("pedidoCalculado")
+    var precioTotal = document.getElementById("pedidoCalculado")
+
+    event.preventDefault()
 
     var total = (precioComida * cantidadComida) + (precioBebida * cantidadBebida) + (precioPostre * cantidadPostre);
     var totalComida = precioComida * cantidadComida;
     var totalBebida = precioBebida * cantidadBebida;
     var totalPostre = precioPostre * cantidadPostre;
 
-    console.log(total)
+    precioTotal.innerHTML = `El precio de sus platos es: $${totalComida} \n El precio de sus bebidas es es: $${totalBebida} \n El precio de sus postres es: $${totalPostre} \n El precio total de su pedido es: $${total}`
 
-    precioTotal.innerHTML = `El precio de sus platos es: $${totalComida}\nEl precio de sus bebidas es es: $${totalBebida}\nEl precio de sus postres es: $${totalPostre}\nEl precio total de su pedido es: $${total}`
+    
 }
