97c30ec09b6af50e3d5a5aa493bc9657b011c1ef
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-10-23 21:39:54 -0500

    Update listas.py

--- Python/Actividades Complementaria Python/Actividad Listas/listas.py
+++ Python/Actividades Complementaria Python/Actividad Listas/listas.py
@@ -1,11 +1,15 @@
 
 #1
+#Aprendices insertados manualmente para que el codigo funcione
 aprendices = ["Sebastian Tovar","Juan Mahecha","Maria Buenaventura","Mathew Guarnizo","Kevin Hernan","Melissa Lopez","Vanesa Ladino","Stiven Ramirez","Esteban Prada","Kevin Botero","Sebastian Pinzon","Camila Alape","Kevin Londoño","Nicolas Fierro","Santiago Gomez","Marlon Lopez","Stiwar Perez","Miguel Bernal","Maria Molano","Nicolas Paulo","Laura Benavides","Wilfrank Bermudez","Dahiana Rivera","Paula Garcia","Maria Jose Perez","Nataly Montaña","Lina Barrios","Cristian Peña","Yency Quiñonez"]
 
+#Edades de los aprendices insertados manualmente para que el codigo funcione
 edadesAprendices = [17,17,17,17,17,16,17,18,17,17,17,18,23,18,17,19,18,17,18,17,17,18,18,19,17,17,18,17,17]
 
 
-#Codigo para insertar los aprendices manualmente
+#*Codigo para insertar los aprendices manualmente
+# Aprendices = []
+# EdadesAprendices = []
 # count = 0
 
 # while count < 30:
@@ -18,20 +22,24 @@ edadesAprendices = [17,17,17,17,17,16,17,18,17,17,17,18,23,18,17,19,18,17,18,17,
 #     count += 1
 
 
-#2
+#2. Mostrar los aprendices y las edades de los aprendices 
 print(f"\nAprendices: {aprendices}\n")
 print(f"Edades de los aprendices: {edadesAprendices}\n")
 
 
-#3
+#3. Muestre el nombre del aprendiz con mayor edad
+#Se selecciona el aprendiz con mayor edad
 edadMayor = max(edadesAprendices)
+#Se selecciona el indice de ese aprendiz
 iAprendizMayor = edadesAprendices.index(edadMayor)
+#Se selecciona el nombre en base al indice de la edad
 aprendizMayor = aprendices[iAprendizMayor]
 
+#Se muestra el aprendiz
 print(f"El aprendiz con mayor edad es {aprendizMayor} con {edadMayor} años\n")
 
 
-#4
+#4. Muestre el nombre de la instructora en posición 1 (0)
 aprendices.insert(0,"Adriana Rincon")
 edadesAprendices.insert(0,35) #!edad inventada :3
 
@@ -39,13 +47,13 @@ print(f"Aprendices: {aprendices}\n")
 print(f"Edades de los aprendices: {edadesAprendices}\n")
 
 
-#5
+#5.Cuente cuantos aprendices tienen 18 años
 aprendicesCon18 = edadesAprendices.count(18)
 
 print(f"{aprendicesCon18} aprendices tienen 18 años\n")
 
 
-#6
+#6. Agregue a un aprendiz al final de la lista
 aprendices.append("Matias Guzman")
 edadesAprendices.append(17)
 
@@ -53,7 +61,7 @@ print(f"Aprendices: {aprendices}\n")
 print(f"Edades de los aprendices: {edadesAprendices}\n")
 
 
-#7
+#7. Borre el nombre de la instructora de la lista
 aprendices.remove(aprendices[0])
 edadesAprendices.remove(edadesAprendices[0])
 
@@ -61,32 +69,32 @@ print(f"Aprendices: {aprendices}\n")
 print(f"Edades de los aprendices: {edadesAprendices}\n")
 
 
-#8
-nombreAprendiz = str(input("Indique el nombre del aprendiz a buscar: "))
-coincidencias = 0
+#8. Indique un dato a buscar en la lista de aprendices
+nombreAprendiz = str(input("Indique el nombre del aprendiz a buscar: ")) #se crea un input para escribir el nombre del aprendiz
+coincidencias = 0 #se crea un contador 
 
-for aprendiz in aprendices:
+for aprendiz in aprendices: #se hace un bucle donde se valida si el nombre del aprendiz se encuentra en la lista
     if nombreAprendiz == aprendiz:
-        print(f"\nEl estudiante {nombreAprendiz} si se encuentra en la lista de estudiantes\n")
+        print(f"\nEl estudiante {nombreAprendiz} si se encuentra en la lista de estudiantes\n") #si si se encuentra
         coincidencias = 1
     else:
-        continue
+        continue #si no se encuentra (continue para evitar error)
 
-if coincidencias == 0:
+if coincidencias == 0: #si el contador no subió
     print(f"\nEl estudiante {nombreAprendiz} no se encuentra en la lista de estudiantes\n")
     
 
 
-#9
-print(f"Los 10 primeros aprendices de la lista son: {aprendices[0:10]}\n")
+#9. Muestre los primeros 10 aprendices de la lista
+print(f"Los 10 primeros aprendices de la lista son: {aprendices[0:10]}\n") #se selecciona el indice del 0 al 10
 print(f"Las edades de 10 primeros aprendices son: {edadesAprendices[0:10]}\n")
 
 
-#10
-print(f"Los 10 ultimos aprendices de la lista son: {aprendices[20:30]}\n")
+#10. Muestre los ultimos 10 aprendices de la lista
+print(f"Los 10 ultimos aprendices de la lista son: {aprendices[20:30]}\n") #se selecciona el indice del 20 al 30
 print(f"Las edades de los 10 ultimos apredices son: {edadesAprendices[20:30]}\n")
 
 
 #11
-print(f"Los aprendices del 10 al 20 en la lista son: {aprendices[10:21]}\n")
+print(f"Los aprendices del 10 al 20 en la lista son: {aprendices[10:21]}\n") #se selecciona el indice del 10 al 21
 print(f"Las edades de los aprendices del 10 al 20 son: {edadesAprendices[10:21]}\n")
