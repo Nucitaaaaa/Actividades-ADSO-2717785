8705a1dd8b4bd780144191ab2c9e28f0cbb3d6f2
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-11-14 11:48:56 -0500

    Update app.js

--- JavaScript/Actividades Complementaria JS/Actividad 4 - JavaScript/js/app.js
+++ JavaScript/Actividades Complementaria JS/Actividad 4 - JavaScript/js/app.js
@@ -1,79 +1,108 @@
 
+//Se declaran las funciones 
+
+//Función suma 
 function suma(n1,n2) {
-    var resultado = n1 + n2;
-    var res = document.getElementById("resultado");
-    //var respuesta = document.getElementById("resultado");
+    // se calcula la suma de los dos numeros 
+    var resultado = n1 + n2; 
+    var res = document.getElementById("resultado"); // Se obtiene el elemento HTML donde se mostrará el resultado
 
-    if(n1 && n2 != 0){
-        respuesta.style.display="block";
-        res.innerHTML = `Resultado: ${resultado}`;
-    }else {
-        respuesta.style.display="block";
-        res.innerHTML = `No valido`;
+    if(n1 && n2 != 0){ // Se verifica que ambos números no sean 0
+
+        // Se muestra el resultado y se cambia el estilo del elemento a bloque
+        respuesta.style.display="block"; 
+        res.innerHTML = `Resultado: ${resultado}`; 
+
+    } else{ // Si alguno de los números es 0, se muestra "No valido"
+        respuesta.style.display="block"; 
+        res.innerHTML = `No valido`; 
     }
 }
 
-function resta(n1,n2) {
+// Función resta
+function resta(n1, n2) {
+    // Se calcula la resta de los dos números
     var resultado = n1 - n2;
+    
+    // Se obtiene el elemento HTML donde se mostrará el resultado
     var res = document.getElementById("resultado");
 
-    if(n1 && n2 != 0){
-        respuesta.style.display="block";
+    // Se verifica que ambos números no sean 0
+    if (n1 && n2 != 0) {
+        // Se muestra el resultado y se cambia el estilo del elemento a bloque
+        respuesta.style.display = "block";
         res.innerHTML = `Resultado: ${resultado}`;
-    }else {
-        respuesta.style.display="block";
+    } else {
+        // Si alguno de los números es 0, se muestra "No valido"
+        respuesta.style.display = "block";
         res.innerHTML = `No valido`;
     }
 }
 
-function multiplicacion(n1,n2) {
+// Función multiplicacion
+function multiplicacion(n1, n2) {
+    // Se calcula la multiplicación de los dos números
     var resultado = n1 * n2;
     var res = document.getElementById("resultado");
 
-    if(n1 && n2 != 0){
-        respuesta.style.display="block";
+    // Se verifica que ambos números no sean 0
+    if (n1 && n2 != 0) {
+        // Se muestra el resultado y se cambia el estilo del elemento a bloque
+        respuesta.style.display = "block";
         res.innerHTML = `Resultado: ${resultado}`;
-    }else {
-        respuesta.style.display="block";
+    } else {
+        // Si alguno de los números es 0, se muestra "No valido"
+        respuesta.style.display = "block";
         res.innerHTML = `No valido`;
     }
 }
 
-function division(n1,n2) {
+// Función division
+function division(n1, n2) {
+    // Se calcula la división de los dos números
     var resultado = n1 / n2;
     var res = document.getElementById("resultado");
 
-    if(n1 && n2 != 0){
-        respuesta.style.display="block";
+    // Se verifica que ambos números no sean 0
+    if (n1 && n2 != 0) {
+        // Se muestra el resultado y se cambia el estilo del elemento a bloque
+        respuesta.style.display = "block";
         res.innerHTML = `Resultado: ${resultado}`;
-    }else {
-        respuesta.style.display="block";
+    } else {
+        // Si alguno de los números es 0, se muestra "No valido"
+        respuesta.style.display = "block";
         res.innerHTML = `No valido`;
     }
 }
 
+// Función potencia
 function potencia(n1) {
-    var resultado = n1*n1;
+    // Se calcula el cuadrado del número
+    var resultado = n1 * n1;
     var res = document.getElementById("resultado");
 
-    if(n1 != 0){
-        respuesta.style.display="block";
+    // Se verifica que el número no sea 0
+    if (n1 != 0) {
+        // Se muestra el resultado y se cambia el estilo del elemento a bloque
+        respuesta.style.display = "block";
         res.innerHTML = `Resultado: ${resultado}`;
-    }else {
-        respuesta.style.display="block";
+    } else {
+        // Si el número es 0, se muestra "No valido"
+        respuesta.style.display = "block";
         res.innerHTML = `No valido`;
     }
 }
 
-function mostrar() {
+//Funcion para mostrar el resultado 
+function mostrar() { //se crea la función mostrar y se le asignan dos variables que contienen los numeros y una para la opción 
     let n1 = parseInt(document.getElementById("n1").value)
     let n2 = parseInt(document.getElementById("n2").value)
     let opc = parseInt(document.getElementById("opc").value)
 
-    switch(opc) {
+    switch(opc) { //se valida la opció que elegio el usuario y se llama a una u otra función dependiendo de esta
         case 1:
             suma(n1,n2)
-            break;
+            break; 
         case 2:
             resta(n1,n2)
             break;
@@ -87,6 +116,6 @@ function mostrar() {
             potencia(n1)
             break;
         default:
-            alert("No valido")
+            alert("No valido") //en caso de que se elija una opción no valida
     }
 }
