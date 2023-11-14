b5ed732394b481443cb823b579f65109116c6598
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-11-14 11:53:41 -0500

    Update app.js

--- JavaScript/Actividades Complementaria JS/Actividad 4 - JavaScript/js/app.js
+++ JavaScript/Actividades Complementaria JS/Actividad 4 - JavaScript/js/app.js
@@ -3,13 +3,13 @@
 
 //Función suma 
 function suma(n1,n2) {
-    // se calcula la suma de los dos numeros 
+    // se calcula la suma de los dos numeros (parametros)
     var resultado = n1 + n2; 
     var res = document.getElementById("resultado"); // Se obtiene el elemento HTML donde se mostrará el resultado
 
     if(n1 && n2 != 0){ // Se verifica que ambos números no sean 0
 
-        // Se muestra el resultado y se cambia el estilo del elemento a bloque
+        // Se muestra el resultado (la variable) y se cambia el estilo del elemento a bloque
         respuesta.style.display="block"; 
         res.innerHTML = `Resultado: ${resultado}`; 
 
@@ -21,7 +21,7 @@ function suma(n1,n2) {
 
 // Función resta
 function resta(n1, n2) {
-    // Se calcula la resta de los dos números
+    // Se calcula la resta de los dos números (parametros)
     var resultado = n1 - n2;
     
     // Se obtiene el elemento HTML donde se mostrará el resultado
@@ -29,7 +29,7 @@ function resta(n1, n2) {
 
     // Se verifica que ambos números no sean 0
     if (n1 && n2 != 0) {
-        // Se muestra el resultado y se cambia el estilo del elemento a bloque
+        // Se muestra el resultado (la variable) y se cambia el estilo del elemento a bloque
         respuesta.style.display = "block";
         res.innerHTML = `Resultado: ${resultado}`;
     } else {
@@ -41,13 +41,13 @@ function resta(n1, n2) {
 
 // Función multiplicacion
 function multiplicacion(n1, n2) {
-    // Se calcula la multiplicación de los dos números
+    // Se calcula la multiplicación de los dos números (parametros)
     var resultado = n1 * n2;
     var res = document.getElementById("resultado");
 
     // Se verifica que ambos números no sean 0
     if (n1 && n2 != 0) {
-        // Se muestra el resultado y se cambia el estilo del elemento a bloque
+        // Se muestra el resultado (la variable) y se cambia el estilo del elemento a bloque
         respuesta.style.display = "block";
         res.innerHTML = `Resultado: ${resultado}`;
     } else {
@@ -59,13 +59,13 @@ function multiplicacion(n1, n2) {
 
 // Función division
 function division(n1, n2) {
-    // Se calcula la división de los dos números
+    // Se calcula la división de los dos números (parametros)
     var resultado = n1 / n2;
     var res = document.getElementById("resultado");
 
     // Se verifica que ambos números no sean 0
     if (n1 && n2 != 0) {
-        // Se muestra el resultado y se cambia el estilo del elemento a bloque
+        // Se muestra el resultado (la variable) y se cambia el estilo del elemento a bloque
         respuesta.style.display = "block";
         res.innerHTML = `Resultado: ${resultado}`;
     } else {
@@ -77,13 +77,13 @@ function division(n1, n2) {
 
 // Función potencia
 function potencia(n1) {
-    // Se calcula el cuadrado del número
+    // Se calcula el cuadrado del número (parametro)
     var resultado = n1 * n1;
     var res = document.getElementById("resultado");
 
     // Se verifica que el número no sea 0
     if (n1 != 0) {
-        // Se muestra el resultado y se cambia el estilo del elemento a bloque
+        // Se muestra el resultad (la variable) o y se cambia el estilo del elemento a bloque
         respuesta.style.display = "block";
         res.innerHTML = `Resultado: ${resultado}`;
     } else {
@@ -99,7 +99,7 @@ function mostrar() { //se crea la función mostrar y se le asignan dos variables
     let n2 = parseInt(document.getElementById("n2").value)
     let opc = parseInt(document.getElementById("opc").value)
 
-    switch(opc) { //se valida la opció que elegio el usuario y se llama a una u otra función dependiendo de esta
+    switch(opc) { //se valida la opción que elegio el usuario y se llama a una u otra función dependiendo de esta
         case 1:
             suma(n1,n2)
             break; 
