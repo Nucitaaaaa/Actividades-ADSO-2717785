d3cf9ecfef2fcfdbde515ca5d6c5c7b630e402ca
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-11-20 22:31:46 -0500

    comentar activivades contador y libreria

--- JavaScript/Actividades Complementaria JS/Actividad 6 - JavaScript/js/app.js
+++ JavaScript/Actividades Complementaria JS/Actividad 6 - JavaScript/js/app.js
@@ -1,17 +1,21 @@
 
-var count = 0;
-const contador = document.getElementById("contador");
+//Declaracion de variables y constantes
+var count = 0; //contador
+const contador = document.getElementById("contador"); //div contador
 
+//Funcion aumento del contador (aumenta en 1)
 function aumento(){
     count += 1;
     contador.innerHTML = count;
 }
 
+//Funcion decremento del contador (disminuye en 1)
 function decremento(){
     count <= 0 ? count = 0 : count -= 1;
     contador.innerHTML = count;
 }
 
+//Funcion reiniciar contador (vuelve a 0)
 function refrescar(){
     count = 0;
     contador.innerHTML = count;
--- JavaScript/Actividades Complementaria JS/Actividad 7 - JavaScript/js/app.js
+++ JavaScript/Actividades Complementaria JS/Actividad 7 - JavaScript/js/app.js
@@ -1,38 +1,46 @@
 
-
-var divTotal = document.getElementById('comprar__total')
+//Declaración de variables (Traer elementos HTML con el DOM)
+var divTotal = document.getElementById('comprar__total') 
 const total = document.getElementById('total')
 const contador1 = document.getElementById("contador1");
 const contador2 = document.getElementById("contador2");
+
+//Variables de js (contadores)
 var count1 = 1;
 var count2 = 1;
 
+//Precios de los libros
 const precioLPy = 70000
 const precioLJs = 85000
 
+//Funcion aumento del contador (Python)
 function aumento1(){
     count1 += 1;
     contador1.innerHTML = `<span>Cantidad:</span> ${count1}`;
 }
 
+//Funcion decremento del contador (Python)
 function decremento1(){
     count1 <= 1 ? count1 = 1 : count1 -= 1;
     contador1.innerHTML = `<span>Cantidad:</span>  ${count1}`;
 }
 
+//Funcion aumento del contador (JavaScript)
 function aumento2(){
     count2 += 1;
     contador2.innerHTML = `<span>Cantidad:</span>  ${count2}`;
 }
 
+//Funcion decremento del contador (JavaScript)
 function decremento2(){
     count2 <= 1 ? count2 = 1 : count2 -= 1;
     contador2.innerHTML = `<span>Cantidad:</span>  ${count2}`;
 }
 
+//Funcion para realizar la compra
 function comprar(){
-    let precioTotal  = (precioLPy * count1) + (precioLJs * count2)
+    let precioTotal  = (precioLPy * count1) + (precioLJs * count2) //suma el precio de la cantidad solicitada de ambos libros
     
-    divTotal.style.display = "block"
-    total.innerHTML = `El total es de: $${precioTotal}`
+    divTotal.style.display = "block" //cambia la vista del div de none a block 
+    total.innerHTML = `El total es de: $${precioTotal}` //Texto que muestra el total del "Pedido"
 }
