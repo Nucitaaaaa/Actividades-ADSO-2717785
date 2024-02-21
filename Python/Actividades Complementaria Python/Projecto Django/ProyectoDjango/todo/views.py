2317054d06974248ce2b24cc460f84af7d7bf68d
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2024-02-21 09:06:03 -0500

    changes

--- Python/Actividades Complementaria Python/Projecto Django/ProyectoDjango/todo/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Tarea
-
-# Create your views here.
-def home(request):
-    tareas = Tarea.objects.all()
-    context = {'tareas':tareas}
-    return render(request, 'todo/home.html', context)
