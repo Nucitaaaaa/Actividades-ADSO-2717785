53c6e32c2668ed5c939341ed3c7664c9388ee21c
SL234 <123673043+Nucitaaaaa@users.noreply.github.com>
2024-02-28 11:54:08 -0500

    update actividad1

--- Python/Actividades Complementaria Python/4 Trimestre/Django/proyecto1/actividad1/models.py
+++ Python/Actividades Complementaria Python/4 Trimestre/Django/proyecto1/actividad1/models.py
@@ -1,3 +1,17 @@
+
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(default='null')
+    public = models.BooleanField()
+    create_date = models.DateTimeField(auto_now_add=True) #Para obtener la fecha de creacion
+    update_date = models.DateTimeField(auto_now = True) #Para obtener la fecha de actualización
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    create_date = models.DateField()
