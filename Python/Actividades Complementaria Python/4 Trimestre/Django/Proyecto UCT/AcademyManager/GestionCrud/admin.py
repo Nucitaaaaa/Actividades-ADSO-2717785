797e92d68b398c305240e3639f236a3c53216249
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2024-03-17 23:01:06 -0500

    Update Proyecto UCT
    - Crear forms de Estudiante, Profesor, Materia y Carrera
    - Configurar Panel de administración

--- Python/Actividades Complementaria Python/4 Trimestre/Django/Proyecto UCT/AcademyManager/GestionCrud/admin.py
+++ Python/Actividades Complementaria Python/4 Trimestre/Django/Proyecto UCT/AcademyManager/GestionCrud/admin.py
@@ -1,3 +1,11 @@
+
 from django.contrib import admin
+from .models import Estudiante, Profesor, Carrera, Materia
 
 # Register your models here.
+class EstudianteAdmin(admin.ModelAdmin):
+    readonly_fields=('estFechaCreacion', 'estFechaActualizacion')
+    admin.site.register(Estudiante) #EstudianteAdmin
+    admin.site.register(Profesor) 
+    admin.site.register(Carrera) 
+    admin.site.register(Materia) 
--- Python/Actividades Complementaria Python/4 Trimestre/Django/Proyecto UCT/AcademyManager/GestionCrud/models.py
+++ Python/Actividades Complementaria Python/4 Trimestre/Django/Proyecto UCT/AcademyManager/GestionCrud/models.py
@@ -2,39 +2,59 @@
 from django.db import models
 
 class Estudiante(models.Model):
-    estNombre = models.CharField( max_length = 30 )
-    estApellido = models.CharField( max_length = 50 )
+    estNombre = models.CharField(max_length=30)
+    estApellido = models.CharField(max_length=50)
     estEmail = models.EmailField()
-    estTelefono = models.IntegerField( max_length = 10 )
+    estTelefono = models.IntegerField(max_length=10)
     estFechaNacimiento = models.DateField()
     estFoto = models.ImageField()
-    estFechaCreacion = models.DateTimeField(auto_now_add = True)
-    estFechaActualizacion = models.DateTimeField(auto_now = True)
-    estCarrera = models.CharField( max_length = 50 )
+    estFechaCreacion = models.DateTimeField(auto_now_add=True)
+    estFechaActualizacion = models.DateTimeField(auto_now=True)
+    estCarrera = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name="Estudiante"
+        verbose_name_plural="Estudiantes"
+        ordering=['id']
 
 class Profesor(models.Model):
-    profNombre = models.CharField( max_length = 30 )
-    profApellido = models.CharField( max_length = 50 )
+    profNombre = models.CharField(max_length=30)
+    profApellido = models.CharField(max_length=50)
     profEmail = models.EmailField()
-    profTelefono = models.IntegerField( max_length = 10 )
+    profTelefono = models.IntegerField(max_length=10)
     profFechaNacimiento = models.DateField()
     profFoto = models.ImageField()
-    profFechaCreacion = models.DateTimeField(auto_now_add = True)
-    profFechaActualizacion = models.DateTimeField(auto_now = True)
-    profCMaterias = models.CharField( max_length = 50 )
+    profFechaCreacion = models.DateTimeField(auto_now_add=True)
+    profFechaActualizacion = models.DateTimeField(auto_now=True)
+    profCMaterias = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name="Profesor"
+        verbose_name_plural="Profesores"
+        ordering=['id']
 
 class Carrera(models.Model):
-    carNombre = models.CharField( max_length = 50 )
-    carDuracion = models.CharField( max_length = 50 )
-    carMaterias = models.CharField( max_length = 50 )
+    carNombre = models.CharField(max_length=50)
+    carDuracion = models.CharField(max_length=50)
+    carMaterias = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name="Carrera"
+        verbose_name_plural="Carreras"
+        ordering=['id']
 
 class Materia(models.Model):
-    matNombre = models.CharField( max_length = 50 )
+    matNombre = models.CharField(max_length=50)
     matDescripcion = models.TextField()
-    matCreditos = models.IntegerField( max_length = 5 )
-    matFechaCreacion = models.DateTimeField(auto_now_add = True)
-    matFechaActualizacion = models.DateTimeField(auto_now = True)
-    matCarrera = models.CharField( max_length = 50 )
-    matProfesores = models.CharField( max_length = 30 )
+    matCreditos = models.IntegerField(max_length=5)
+    matFechaCreacion = models.DateTimeField(auto_now_add=True)
+    matFechaActualizacion = models.DateTimeField(auto_now=True)
+    matCarrera = models.CharField(max_length=50)
+    matProfesores = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name="Materia"
+        verbose_name_plural="Materias"
+        ordering=['id']
 
 
