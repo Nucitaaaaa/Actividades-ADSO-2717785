946705fc890598194f59d8f2a82844d462958d2a
SL234 <123673043+Nucitaaaaa@users.noreply.github.com>
2024-03-20 06:22:10 -0500

    Update proyecto1

--- Python/Actividades Complementaria Python/4 Trimestre/Django/proyecto1/actividad1/apps.py
+++ Python/Actividades Complementaria Python/4 Trimestre/Django/proyecto1/actividad1/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class Actividad1Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'actividad1'
+    verbose_name= 'Aplicacion de blog'
--- Python/Actividades Complementaria Python/4 Trimestre/Django/proyecto1/actividad1/models.py
+++ Python/Actividades Complementaria Python/4 Trimestre/Django/proyecto1/actividad1/models.py
@@ -2,7 +2,11 @@
 from django.db import models
 
 class Article(models.Model):
-    
+
+    class Meta:
+        verbose_name = "Articulo"
+        verbose_name_plural = "Articulos"
+        
     title = models.CharField(max_length=100)
     content = models.TextField()
     #image = models.ImageField(default='null')
@@ -11,6 +15,10 @@ class Article(models.Model):
     update_date = models.DateTimeField(auto_now = True) #Para obtener la fecha de actualización
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name = "Articulo"
+        verbose_name_plural = "Articulos"
 
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
