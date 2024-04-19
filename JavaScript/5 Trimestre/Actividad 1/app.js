d415bce0e2001c867fabca467789c4a785bb16dd
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2024-04-19 15:04:57 -0500

    Update actividad 1

--- JavaScript/5 Trimestre/Actividad 1/app.js
+++ JavaScript/5 Trimestre/Actividad 1/app.js
@@ -1,10 +1,20 @@
 
 //*Ejercicio 1  
-//!falta
 function pi() {
+    let divPi= document.getElementById("pi");
+    let buttonPi= document.getElementById("buttonPi");
     let PI = Math.PI;
+    let roundedPi = PI.toFixed(2)
 
-
+    if(divPi.innerText == PI){
+        divPi.innerText = `${roundedPi}`;
+        buttonPi.innerText = `No Redondear`
+    }
+    else{
+        divPi.innerText = `${PI}`;
+        buttonPi.innerText = `Redondear`
+    }
+    
 } 
 
 
@@ -50,3 +60,43 @@ function reverseWord(word) {
 
 
 //*Ejercicio 4
+function randomNum1(min, max) {
+    let divNum1 = document.getElementById("randomNum1");
+    min =  parseInt(document.getElementById("min").value);
+    max =  parseInt(document.getElementById("max").value);
+
+    if(min < max){
+        let randomNumber = Math.floor(Math.random() * (max  - min) + min);
+        divNum1.innerText = `${randomNumber}`;
+    }
+    else{
+        divNum1.innerText = `El numero minimo debe ser menor al numero maximo`;
+    }
+}
+
+
+//*Ejercicio 5
+function randomNum2() {
+    let divNum2 = document.getElementById("randomNum2");
+
+    let min = 1;
+    let max = 1000;
+    let randomNums = [];
+    let finalNums = ""
+
+    while(randomNums.length < 100) {
+        let randomNumber = Math.floor(Math.random() * (max  - min) + min);
+        let findNum = randomNums.find(num => num === randomNumber);
+
+        if(findNum == undefined) {
+            randomNums.push(randomNumber);
+        }
+    }
+
+    for(num of randomNums){
+        finalNums += `${num},`
+    }
+    
+    console.log(randomNums)
+    divNum2.innerText = `${finalNums}.`;
+}
