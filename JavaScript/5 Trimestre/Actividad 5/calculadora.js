349ca3c0b837f8378ca01039d9f5ceee21dc856a
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2024-04-26 19:37:35 -0500

    Update Actividad 5

---
+++ JavaScript/5 Trimestre/Actividad 5/calculadora.js
@@ -0,0 +1,37 @@
+
+
+function agregarValor(valor) {
+    let operacionContenedor = document.getElementById("operacion");
+    operacionContenedor.textContent += `${valor}`; 
+}
+
+function calcular(){
+    let operacionContenedor = document.getElementById("operacion")
+    let operacion = document.getElementById("operacion").textContent;
+
+    operacion = operacion.replace(/÷/g, '/'); 
+    operacion = operacion.replace(/×/g, '*'); 
+    
+    try{
+        let resultado = eval(operacion);
+        operacionContenedor.textContent = `${resultado}`
+    }
+    catch(error){
+        operacionContenedor.textContent = `Syntax ERROR`
+    }
+
+}
+
+function borrar(){
+    let operacionContenedor = document.getElementById("operacion")
+    let operacion = document.getElementById("operacion").textContent;
+
+    let nuevaOperacion = operacion.substring(0, operacion.length - 1);
+    operacionContenedor.textContent = `${nuevaOperacion}`
+}
+
+function borrarTodo(){
+    let operacionContenedor = document.getElementById("operacion")
+    
+    operacionContenedor.textContent = '' 
+}
