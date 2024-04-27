23ee0cda35bbdaa8aa1275252e4523e3a7089a72
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2024-04-26 22:46:33 -0500

    Update Actividad 5

---
+++ JavaScript/5 Trimestre/Actividad 5/reloj.js
@@ -0,0 +1,29 @@
+
+function mostrarHora() {
+    const dia = new Date();
+    let hora = dia.getHours();
+    let minutos = dia.getMinutes();
+    let segundos = dia.getSeconds();
+    let horario = hora >= 12 ? 'PM' : 'AM';
+
+    hora = hora % 12 || 12; //convertir de 24 a 12 h
+
+    hora < 10 ? hora = '0' + hora : hora = hora
+    minutos < 10 ? minutos = '0' + minutos : minutos = minutos
+    segundos < 10 ? segundos = '0' + segundos : segundos = segundos
+
+    const horaActual = `${hora}:${minutos}:${segundos} ${horario}`;
+
+    document.getElementById('reloj').textContent = horaActual;
+}
+
+setInterval(mostrarHora, 1000);
+mostrarHora();
+
+function cronometro() {
+    
+}
+
+function temporizador() {
+
+}
