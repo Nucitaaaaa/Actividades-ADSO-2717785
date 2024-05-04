e6d7a58dc58ed2a9c58eea7e6d2c36ba5f448064
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2024-05-03 22:51:35 -0500

    Update Actividad 5

--- JavaScript/5 Trimestre/Actividad 5/reloj.js
+++ JavaScript/5 Trimestre/Actividad 5/reloj.js
@@ -3,13 +3,13 @@ function mostrarHora() {
     const dia = new Date();
     let hora = dia.getHours();
     let minutos = dia.getMinutes();
-    let horario = hora >= 12 ? 'p.m' : 'a.m';
+    // let horario = hora >= 12 ? 'P.M' : 'A.M';
 
     hora = hora % 12 || 12; //convertir de 24 a 12 h
 
     minutos < 10 ? minutos = '0' + minutos : minutos = minutos
 
-    const horaActual = `${hora}:${minutos} ${horario}`;
+    const horaActual = `${hora}:${minutos}`;
 
     document.getElementById('reloj').textContent = horaActual;
 }
