f75ca20bc781834a6e082238b628dfec39dc7148
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2024-05-29 10:25:46 -0500

    Update act1 node

--- JavaScript/Node.js/Actividad Lista De Tareas/client.js
+++ JavaScript/Node.js/Actividad Lista De Tareas/client.js
@@ -1,4 +1,42 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+    agregarTarea();
+});
+
+function agregarTarea() {
+    document.getElementById('formulario-tarea').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const nombre = document.getElementById('nombre').value;
+        const contenido = document.getElementById('contenido').value;
+    
+        const tarea = { nombre, contenido };
+    
+        fetch('http://localhost:3030/tareas', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(tarea)
+        })
+        .then(response => response.json())
+        .then(data => {
+            agregarTareaALista(data);
+        })
+        .catch(error => console.error('Error:', error));
+    });
+}
+
+function agregarTareaALista(tarea) {
+    const tareasElement = document.getElementById('tareas');
+    const element = document.createElement('li');
+    const content = document.createTextNode(`ID: ${tarea.id}, Nombre: ${tarea.nombre}, Contenido: ${tarea.contenido}`);
+    
+    element.appendChild(content);
+    tareasElement.appendChild(element);
+}
+
+
 function lista() {
     const tareasElement = document.getElementById("tareas"); 
     fetch("http://localhost:3030/tareas")
@@ -8,43 +46,13 @@ function lista() {
 
             data.forEach(function(tarea) { 
                 let element = document.createElement("li");
-                let content = document.createTextNode(`ID: ${tarea.id} , Nombre: ${tarea.nombre} , Contenido: ${tarea.contenido}`);
+                let content = document.createTextNode(`Nombre: ${tarea.nombre} || Contenido: ${tarea.contenido}`);
                 
                 element.appendChild(content);
                 tareasElement.appendChild(element);
             });
         });
 }
+
 lista();
 
-
-document.getElementById('formulario-tarea').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    const nombre = document.getElementById('nombre').value;
-    const contenido = document.getElementById('contenido').value;
-
-    const tarea = { nombre, contenido };
-
-    fetch('http://localhost:3030/tareas', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(tarea)
-    })
-    .then(response => response.json())
-    .then(data => {
-        agregarTareaALista(data);
-    })
-    .catch(error => console.error('Error:', error));
-});
-
-function agregarTareaALista(tarea) {
-    const tareasElement = document.getElementById('tareas');
-    const element = document.createElement('li');
-    const content = document.createTextNode(`ID: ${tarea.id}, Nombre: ${tarea.nombre}, Contenido: ${tarea.contenido}`);
-    
-    element.appendChild(content);
-    tareasElement.appendChild(element);
-}    
